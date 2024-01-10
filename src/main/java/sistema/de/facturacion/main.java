8cbcf98ffbf9105f65887d95c831412c43b9b6f7
bladi23 <bladivela2003@hotmail.com>
2024-01-09 20:54:08 -0500

    Interfaz

--- src/main/java/sistema/de/facturacion/main.java
+++ src/main/java/sistema/de/facturacion/main.java
@@ -1,13 +1,16 @@
 package sistema.de.facturacion;
 
+import interfaz.Login;
+
 /**
  * Hello world!
  *
  */
 public class main 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new Login().setVisible(true);  // Crea e muestra la ventana de inicio de sesión
+        });
     }
 }
